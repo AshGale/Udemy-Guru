cf7cad5a387b472cdda6a82972dd5f05418d89a6
AshGale <ashgale17@yahoo.co.uk>
2018-10-18 09:39:04 +0100

    added crud repo for books and authors

---
+++ src/main/java/com/spring/boot/webappguru/repositories/AuthorRespository.java
@@ -0,0 +1,7 @@
+package com.spring.boot.webappguru.repositories;
+
+import com.spring.boot.webappguru.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public class AuthorRespositories extends CrudRepository<Author, Long> {
+}
---
+++ src/main/java/com/spring/boot/webappguru/repositories/BookRepository.java
@@ -0,0 +1,4 @@
+package com.spring.boot.webappguru.repositories;
+
+public interface BookRepository {
+}
